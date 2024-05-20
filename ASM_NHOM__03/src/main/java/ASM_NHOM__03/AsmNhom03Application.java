e9659636be3c4853b0a83035001374d8da5a3c97
QuocAnh2004 <quocanhng2804@gmail.com>
2024-05-20 21:01:49 +0700

    add project demo 02

---
+++ ASM_NHOM__03/src/main/java/ASM_NHOM__03/AsmNhom03Application.java
@@ -0,0 +1,13 @@
+package ASM_NHOM__03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmNhom03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmNhom03Application.class, args);
+	}
+
+}
---
+++ ASM_NHOM__03/src/test/java/ASM_NHOM__03/AsmNhom03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ASM_NHOM__03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmNhom03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
